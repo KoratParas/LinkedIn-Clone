80ff0bedfdf5cc215c7225e43d0e579d5ea75a9a
ParasK <koratparas@gmail.com>
2024-10-27 21:38:01 +1100

    Task: header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>lets build a LinkedIn Clone</h1>
-
       {/* Header */}
+      <Header />
       {/* App body */}
       {/* sidebar */}
       {/* feed */}
